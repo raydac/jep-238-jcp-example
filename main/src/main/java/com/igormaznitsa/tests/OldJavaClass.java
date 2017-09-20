23d0985650ff1c94b49f11dc57e21d6f6ea2d85c
Igor Maznitsa <rrg4400@gmail.com>
2017-09-20 21:34:20 +0300

    added examples of tests

---
+++ main/src/main/java/com/igormaznitsa/tests/OldJavaClass.java
@@ -0,0 +1,8 @@
+//#excludeif java.version>8
+package com.igormaznitsa.tests;
+
+public class OldJavaClass {
+    public String getString(){
+        return "POJO";
+    }
+}
--- main/src/main/java/com/igormaznitsa/tests/OnlyJava9Class.java
+++ main/src/main/java/com/igormaznitsa/tests/OnlyJava9Class.java
@@ -4,7 +4,7 @@ package com.igormaznitsa.tests;
 import java.util.List;
 
 public class OnlyJava9Class {
-  public List<String> getList() {
-    return List.of("one","two","three");
-  }
+    public List<String> getList() {
+        return List.of("one", "two", "three");
+    }
 }
